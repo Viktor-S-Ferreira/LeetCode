d8a857234b530f2b802311e8ff4338b7bd44c389
Viktor-S-Ferreira <141160595+Viktor-S-Ferreira@users.noreply.github.com>
2025-04-06 18:57:47 -0300

    Time: 59 ms (36.45%), Space: 56.7 MB (15.25%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    var result = 0;
+    if (x < (-Math.pow(2,31)) || x > (Math.pow(2,31) - 1))
+    {  
+        return 0;
+    }
+    while (x != 0){
+        result = x%10 + result*10;
+        x = (x - x%10)/10;
+    }
+    if (result < (-Math.pow(2,31)) || result > (Math.pow(2,31) - 1))
+    {  
+        return 0;
+    }
+    return result;
+};
