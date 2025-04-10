0e443265bffa1c0d2576ed4c3303d32171f67025
Viktor-S-Ferreira <141160595+Viktor-S-Ferreira@users.noreply.github.com>
2025-04-10 11:20:33 -0300

    Time: 9 ms (7.91%), Space: 66.5 MB (18.51%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -6,12 +6,23 @@
 var rotate = function(nums, k) {
     let aux = new Array();
     k = k%(nums.length);
-    aux = nums.splice(nums.length - k,k);
-    //console.log(aux);
+    //aux = nums.splice(nums.length - k,k);
     for(let i = 0; i < k; i++){
-        //nums.unshift(nums.pop());
-        //console.log("aux[k-i] = " + aux[k-i-1]);
-        nums.unshift(aux[k-i-1]);
+        aux[i] = nums[nums.length - k + i];
+        //console.log(aux);
+        //nums.unshift(aux[i]);
+        //console.log(nums);
+    }
+    let aux2 = [];
+    for(let i = 0; i < nums.length; i++){
+        if(i < k){
+            aux2[i] = aux[i];
+        } else {
+            aux2[i] = nums[i-k];
+        }
+    }
+    for(let i = 0; i < nums.length; i++){
+        nums[i] = aux2[i];
     }
     
 };
